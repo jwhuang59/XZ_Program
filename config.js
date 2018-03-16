cd9dea824bb5e5ee38cb0483ea21ff5fd793b798
jwhuang59 <33829699+jwhuang59@users.noreply.github.com>
2018-03-16 19:54:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,23 +7,23 @@ var config = {
 
   // 下面的地址配合云端 Server 工作
   host,
-  domain: `http://${host}`,
+  domain: `https://${host}`,
   //设置图片地址
-  pic: `http://${host}/admin/img-list/`,
+  pic: `https://${host}/admin/img-list/`,
   //个人简历
-  resume: `http://${host}/view/resume/`,
+  resume: `https://${host}/view/resume/`,
   //头像地址
-  head_pic_url: `http://${host}/static/images/head_pic.jpg`,
+  head_pic_url: `https://${host}/static/images/head_pic.jpg`,
   //文章数据
-  all_article_url: `http://${host}/wechat/all_article/`,
+  all_article_url: `https://${host}/wechat/all_article/`,
   //分类列表
-  article_list_url: `http://${host}/wechat/categorys/`,
+  article_list_url: `https://${host}/wechat/categorys/`,
   //留言列表
-  feedback_list_url: `http://${host}/wechat/feedback_list/`,
+  feedback_list_url: `https://${host}/wechat/feedback_list/`,
   //文章详情
-  article_detail_url: `http://${host}/wechat/article_detail/`,
+  article_detail_url: `https://${host}/wechat/article_detail/`,
   //留言添加
-  feedback_add_url: `http://${host}/wechat/feedback_add/`,
+  feedback_add_url: `https://${host}/wechat/feedback_add/`,
 };
 
 module.exports = config
